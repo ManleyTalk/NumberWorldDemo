4b214b4645b431bd4ba629a22bfe302f748adcf1
manley <manleytalk@hotmail.com>
2019-03-24 10:19:27 +0800

    添加双精度和固定点类型

--- NumberWorldDemo/Program.cs
+++ NumberWorldDemo/Program.cs
@@ -6,7 +6,58 @@ namespace NumberWorldDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 24;
+            int b = 8;
+
+            int c = a + b;//加法运算
+            //Console.WriteLine(c);
+
+            ///////////////////////////////////
+
+            c = a - b;//减法运算
+           // Console.WriteLine(c);
+
+            c = a * b;//乘法运算
+           // Console.WriteLine(c);
+
+            c = a / b;//除法运算
+           // Console.WriteLine(c);
+
+            c = a % b;//求余运算
+            //Console.WriteLine(c);
+
+            ////////////////////////////////////
+
+            int d = 2;
+
+            int e = a + b * d;
+           // Console.WriteLine(e);
+
+            e = (a + b) * d;
+            //Console.WriteLine(e);
+
+            /////////////////////////////////
+
+            c = 5 / 3;
+            //Console.WriteLine(c);
+
+            c = 5 % 3;
+            //Console.WriteLine(c);
+
+            double f = 5;
+            double g = 4;
+            double h = 2;
+            double i = (f + g) / h;
+            //Console.WriteLine(i);
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+
+            decimal j = 1.0M / 3.0M;
+            Console.WriteLine(j);
+
+
+            Console.ReadKey();
         }
     }
 }
